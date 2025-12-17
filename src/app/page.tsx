4f947b73a6f48198d15243db6edc059afadfcc05
Viacheslav Liashenko <Viacheslav_Liashenko@comebackmail.com>
2025-12-17 13:31:11 +0200

    uikit/button-icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import { DenyIcon, GoogleIcon, SuccessIcon } from '@/shared/assets/icons';
+import {
+  BellIcon,
+  DenyIcon,
+  GearIcon,
+  GoogleIcon,
+  SuccessIcon,
+} from '@/shared/assets/icons';
 import { Button } from '@/shared/components/ui';
 
 export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-center gap-4 bg-white px-16 py-32 sm:items-start dark:bg-black'>
+        <div className='flex gap-2'>
+          <Button variant={'ghost'} size={'icon'}>
+            <BellIcon />
+          </Button>
+          <Button variant={'ghost'} size={'icon'}>
+            <GearIcon />
+          </Button>
+        </div>
         <Button variant={'default'} size={'default'}>
           Sign in with Google <GoogleIcon />
         </Button>
--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as GoogleIcon } from './google';
 export { default as DenyIcon } from './deny';
 export { default as SuccessIcon } from './success';
+export { default as BellIcon } from './bell';
+export { default as GearIcon } from './gear';
