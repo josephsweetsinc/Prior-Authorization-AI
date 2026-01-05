91675844311a338214329a1d7d4a3f08fc8013bb
mykhailo-hulak <Mykhailo_Hulak@comebackmail.com>
2026-01-05 18:34:17 +0100

    add filter & pipeline utils for user management feature; add user management container component; update shared components

--- src/views/user-management/UserManagement.tsx
+++ src/views/user-management/UserManagement.tsx
@@ -1,5 +1,23 @@
+import { Plus } from 'lucide-react';
+
+import { Button, TitleAndDesc } from '@/shared/components';
+
 const UserManagement = () => {
-  return <div>UserManagement</div>;
+  return (
+    <main className='space-y-6'>
+      <div className='flex flex-wrap items-end justify-between gap-8'>
+        <TitleAndDesc
+          title='User Management'
+          subtitle='Manage system users and their permissions'
+        />
+
+        <Button variant='primary' className='w-max px-16! py-2.5!' disabled>
+          <Plus className='size-4' />
+          <span className='text-base font-medium tracking-wide'>Add User</span>
+        </Button>
+      </div>
+    </main>
+  );
 };
 
 export default UserManagement;
