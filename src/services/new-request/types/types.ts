96b05bdb69f4e239d521390bde33624981b0c9c5
mykhailo-hulak <Mykhailo_Hulak@comebackmail.com>
2026-01-07 19:59:02 +0100

    update new-request  types ; update createRequest hook; add buildBody util

---
+++ src/services/new-request/types/types.ts
@@ -0,0 +1,23 @@
+import { type FormState } from '@/features/new-request';
+
+export interface ICreateRequestParams {
+  fileIds?: number[];
+  form?: FormState | null;
+}
+
+export interface ICreateRequestBody {
+  request_id: number;
+  file_ids: number[];
+  transportation_type: string;
+  patient_first_name: string;
+  patient_last_name: string;
+  patient_date_of_birth: string;
+  patient_id: string;
+  date_of_transport: string;
+  time_of_transport: string;
+  pickup_address: string;
+  destination_address: string;
+  primary_diagnosis: string;
+  medical_justification: string;
+  form_number: string;
+}
