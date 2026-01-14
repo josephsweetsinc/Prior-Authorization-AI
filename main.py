fdb120ac6732e6c6c71844a1a7aabf6de7893d8d
osyshyn <ceo@comebackagency.com>
2026-01-14 19:31:03 +0200

    cors fixed

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://prior-authorization-ai.vercel.app",
-    "http:localhost:8000"
+        "https://prior-authorization-ai.vercel.app",
+        "http:localhost:8000"
     ],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 main_api_router = initialize_routers()
 app.include_router(main_api_router)
