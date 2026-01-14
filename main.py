b2895ae301f5068e62a868cc42f43da0508c2010
osyshyn <ceo@comebackagency.com>
2026-01-14 19:50:00 +0200

    cors fixed

--- main.py
+++ main.py
@@ -21,7 +21,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=[
+        'https://prior-authorization-ai.vercel.app',
+        'http://localhost:8000',
+        'http://127.0.0.1:8000',
+    ],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
