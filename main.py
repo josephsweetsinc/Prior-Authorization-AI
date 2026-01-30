4868008a2b09d0d60f4866323ca0264f727a50c7
osyshyn <ceo@comebackagency.com>
2026-01-30 16:49:19 +0200

    ref(app): add localhost URLs to CORS allowed origins

--- main.py
+++ main.py
@@ -24,11 +24,14 @@ app.add_middleware(
     allow_origins=[
         "https://www.infopaai.com",
         "https://api.infopaai.com",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 main_api_router = initialize_routers()
 app.include_router(main_api_router)
 
